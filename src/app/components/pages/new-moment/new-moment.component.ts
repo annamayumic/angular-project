24bba76edf8abb1b6ceacc7fcc9b96107068a31a
Anna Mayumi <carvalhoannamayumi@gmail.com>
2024-08-04 21:54:52 +0900

    aula 26

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrl: './new-moment.component.css'
+})
+export class NewMomentComponent implements OnInit {
+  btnText="Compartlhar"
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    
+  }
+}
